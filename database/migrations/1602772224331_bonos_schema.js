f84e5336222cc1e153e27a0ce9fd0b3bc5fe4848
Fabian <ingfabianavellaneda@gmail.com>
2020-10-15 21:57:16 -0500

    Se modifican las migraciones en compras

--- database/migrations/1602772224331_bonos_schema.js
+++ database/migrations/1602772224331_bonos_schema.js
@@ -7,7 +7,7 @@ class BonosSchema extends Schema {
   up () {
     this.create('bonos', (table) => {
       table.increments()
-      table.enu('tipo', ['Regalo', 'Recarga', 'Recarga valor']).notNullable()
+      table.enu('tipo', ['Regalo', 'Recarga']).notNullable()
       table.text('contenido', 'longtext').unique().notNullable()
       table.decimal('saldo', 2).notNullable()
       table.integer('cliente_id').unsigned().references('id').inTable('clientes').notNullable()

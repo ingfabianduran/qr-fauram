da105511170c6d3db6726f7871f60e0e064b708d
Fabian Duran <ingfabianavellaneda@gmail.com>
2020-11-09 20:12:50 -0500

    Se incluye la libreria para generar pdf

--- public/js/site/bono.js
+++ public/js/site/bono.js
@@ -11,6 +11,8 @@ $(document).ready(function() {
     get_data_search_cliente();
     // Post form add bono: 
     validate_form_add_bono('form_add_bono', get_rules().bono, '¿Desea registrar el bono?', '/bono/add');
+    
+    generate_pdf();
 });
 // Event lost focus in input identificacion: 
 function get_data_search_cliente() {
@@ -84,4 +86,50 @@ function events_register_cliente(message) {
     toastr.success(message);
     document.getElementById('container_create_bono').classList.remove('d-none');
     document.getElementById('container_info').classList.add('d-none');
+}
+
+function generate_pdf() {
+    const pdf_content = {
+        content: [
+            {
+                text: 'QR Recargable',
+                style: 'type_bono'
+            },
+            {
+                text: 'Hola Fabian Esteban Duran A',
+                style: 'user'
+            },
+            {
+                text: 'Compraste un Código QR que puedes recargar. Ahora puedes usarlo como medio de pago en nuestras instalaciones.',
+                style: 'body'
+            },
+            {
+                text: 'Franklin Ramos Salon \n Cra 13 #77a-65 \n 3132859321',
+                style: 'footer'
+            }
+        ],
+        styles: {
+            type_bono: {
+                fontSize: 30,
+                bold: true               
+            },
+            user: {
+                fontSize: 25,
+                bold: true,
+                margin: [20, 160, 20, 20] // [left, top, right, bottom]
+            },
+            body: {
+                fontSize: 20,
+                alignment: 'justify',
+                margin: [20, 5, 20, 0]
+            },
+            footer: {
+                fontSize: 15,
+                alignment: 'center',
+                margin: [0, 380, 0, 0]
+            }
+        }
+    };
+
+    pdfMake.createPdf(pdf_content).download();
 }
